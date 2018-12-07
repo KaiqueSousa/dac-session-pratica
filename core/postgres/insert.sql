9d36d0a2ed2ffbab1be4b055fcda8283207ddb07
KaiqueSousa <kaique.una@gmail.com>
2018-12-07 14:15:56 -0300

    Organizando projeto

--- core/postgres/insert.sql
+++
@@ -1,5 +0,0 @@
---inserindo os clientes
-INSERT INTO clientes VALUES ('Kiko','123');
-INSERT INTO clientes VALUES ('Chaves','124');
-INSERT INTO clientes VALUES ('Madruga','125');
-INSERT INTO clientes VALUES ('Florinda','127');
